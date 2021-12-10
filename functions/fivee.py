17996d9d76fc6b80d0beb8ae1d49ae63f18c10b2
ieb <1@wa.vg>
2021-12-10 13:27:13 +0800

    fix

--- functions/fivee.py
+++ functions/fivee.py
@@ -35,37 +35,37 @@ async def fiveEMatches(app, message, reply, index=0):
         pass
 
     response += [Plain("\n-------------------")]
+    if five_e_data["match"]:
+        for row in five_e_data["match"][:8]:
+            s1 = int(row["group1_all_score"])
+            s2 = int(row["group2_all_score"])
+            is_team1 = row["group_id"] == "1"
+            team1_score = ("[%d]" if is_team1 else "%d") % s1
+            team2_score = ("[%d]" if not is_team1 else "%d") % s2
+            win = (
+                is_team1 and s1 > s2
+            ) or (
+                not is_team1 and s1 < s2
+            )
+            if s1 == s2:
+                symbol = '💛'
+            elif win:
+                symbol = '💚'
+            else:
+                symbol = '💔'
+            change_elo_text = row['change_elo']
+            change_elo = float(row['change_elo'])
+            base_elo = float(row['origin_elo'])
+            if float(change_elo_text) > 0:
+                change_elo_text = "+" + change_elo_text
 
-    for row in five_e_data["match"][:8]:
-        s1 = int(row["group1_all_score"])
-        s2 = int(row["group2_all_score"])
-        is_team1 = row["group_id"] == "1"
-        team1_score = ("[%d]" if is_team1 else "%d") % s1
-        team2_score = ("[%d]" if not is_team1 else "%d") % s2
-        win = (
-            is_team1 and s1 > s2
-        ) or (
-            not is_team1 and s1 < s2
-        )
-        if s1 == s2:
-            symbol = '💛'
-        elif win:
-            symbol = '💚'
-        else:
-            symbol = '💔'
-        change_elo_text = row['change_elo']
-        change_elo = float(row['change_elo'])
-        base_elo = float(row['origin_elo'])
-        if float(change_elo_text) > 0:
-            change_elo_text = "+" + change_elo_text
+            final_elo = round(base_elo + change_elo, 2)
 
-        final_elo = round(base_elo + change_elo, 2)
-
-        msg = f"\n{symbol} {convertTimeStamp(int(row['start_time']))} {row['map']}\n" \
-              f"{team1_score} - {team2_score} / {row['kill']}-{row['death']} / Rating {row['rating']}"
-        if base_elo > 0:
-            msg += f" Elo: {final_elo} ({change_elo_text})"
-        response += [Plain(msg)]
+            msg = f"\n{symbol} {convertTimeStamp(int(row['start_time']))} {row['map']}\n" \
+                  f"{team1_score} - {team2_score} / {row['kill']}-{row['death']} / Rating {row['rating']}"
+            if base_elo > 0:
+                msg += f" Elo: {final_elo} ({change_elo_text})"
+            response += [Plain(msg)]
 
     return response
 
@@ -96,23 +96,24 @@ async def fiveESignature(app, message, reply, index=0):
     }
 
     results = []
-    for row in five_e_data["match"][:10]:
-        map = row["map"].split("_")[-1]
-        mapLogo = "https://www.csgo.com.cn/images/maps/logo/%s.png" % map
+    if five_e_data["match"]:
+        for row in five_e_data["match"][:10]:
+            map = row["map"].split("_")[-1]
+            mapLogo = "https://www.csgo.com.cn/images/maps/logo/%s.png" % map
 
-        basename = "images/" + os.path.basename(mapLogo)
-        if not os.path.isfile(basename):
-            open(basename, "wb+").write(requests.get(mapLogo).content)
+            basename = "images/" + os.path.basename(mapLogo)
+            if not os.path.isfile(basename):
+                open(basename, "wb+").write(requests.get(mapLogo).content)
 
-        results.append({
-            'map': basename,
-            'timestamp': int(row['end_time']),
-            'score1': int(row['group1_all_score'] if row['group_id'] == "1" else row['group2_all_score']),
-            'score2': int(row['group2_all_score'] if row['group_id'] == "1" else row['group1_all_score']),
-            'rating': float(row['rating']),
-            'elo': float(row['origin_elo']) + float(row['change_elo']),
-            'elo_change': float(row['change_elo']),
-        })
+            results.append({
+                'map': basename,
+                'timestamp': int(row['end_time']),
+                'score1': int(row['group1_all_score'] if row['group_id'] == "1" else row['group2_all_score']),
+                'score2': int(row['group2_all_score'] if row['group_id'] == "1" else row['group1_all_score']),
+                'rating': float(row['rating']),
+                'elo': float(row['origin_elo']) + float(row['change_elo']),
+                'elo_change': float(row['change_elo']),
+            })
 
     return [
         Image.fromUnsafeBytes(create_signature(user_info, results)),
